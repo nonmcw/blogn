e17362c9c31212e9424ef2afd54b55b0dd687d6e
nonmcw <nonmcw@live.com>
2018-04-17 17:42:26 +0800

    rm models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,22 @@
 from . import db
 from datetime import datetime
 
+class Comment(db.Model):
+    __tables__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    disable = db.Column(db.Boolean)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    
+class Follow(db.Model):
+    __tables__ = 'follows'
+    follower_id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    
 class Role(db.Model):
     __tables__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +29,42 @@ class Role(db.Model):
         return '<Role %r>' % self.name
     
 class User(db.Model):
-    __tables__ = 'users'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    pass_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
+    avatar = db.Column(db.String(32))
+    name = db.Column(db.String(64), index=True)
+    phone = db.Column(db.String(16), unique=True)
+    about_me = db.Column(db.Text(), unique=True)
+    location = db.Column(db.String(256), nullable=True)
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship('Follow',
+                               foreign_keys=[Follow.follower_id],
+                               backref=db.backref('follower', lazy='joined'),
+                               lazy='dynamic',
+                               cascade='all, delete-orphan')
+    follower = db.relationship('Follow',
+                               foreign_keys=[Follow.followed_id],
+                               backref=db.backref('followed', lazy='joined'),
+                               lazy='dynamic',
+                               cascade='all, delete-orphan')
+    comments = db.relationship('Comment', backref='author', lazy='dynamic')
+    
+    def __repr__():
+        return '<User %r>' % self.username
+     
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    body = db.Column(db.Text)
+    tag = db.Column(db.String(32), index=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
