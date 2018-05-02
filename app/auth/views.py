9b0dbdb816f8d92b44ddd439fccf6bb7364d9f81
nonmcw <nonmcw@live.com>
2018-05-02 18:00:12 +0800

    update form file

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,16 @@
-from flask import render_template
+from flask import render_template, request, url_for, flash
+from flask_login import login_user
 from . import auth
+from ..models import User
+from .forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.date).first()
+        if user is not None and User.verify_password(form.password.date):
+            login_user(user, form.remember_me.date)
+            redirect(request.args.get('next') or url_for('main.index'))
+        flash('invalid username or password')
+    return render_template('auth/login.html', form=form)
