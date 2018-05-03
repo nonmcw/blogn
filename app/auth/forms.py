d6e7fc7beda8cae6be34be25e2db9cc66e47f342
nonmcw <nonmcw@live.com>
2018-05-03 18:00:41 +0800

    write login and forms file.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,25 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidateError
+from ..models import User
 
-class LoginForm(Form):
-    email = StringField('Email', validators=[Required(), Length(5, 64), Email()])
+class LoginForm(FlaskForm):
+    email = StringField('Email or Username', validators=[Required(), Length(5, 64), Email()])
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('keep me logged in')
     submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Length(5, 64), Email()])
+    username = StringField('Username', validators=[Required(), Length(4, 64), 
+                                   Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                   'Usernames must have ...')
+    password = PasswordField('Password', validators=[Required(),
+                                      EqualTo('password2', massage='Password must match.'))
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('register')
+
+    def validate_email(self, field):
+          if User.query.filter_by(email=field.data).first():
+               return ValidateError('Email areldy registered')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,12 @@
-from flask import render_template, request, url_for, flash
-from flask_login import login_user
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
 from .forms import LoginForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm
+    form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.date).first()
         if user is not None and User.verify_password(form.password.date):
@@ -14,3 +14,10 @@ def login():
             redirect(request.args.get('next') or url_for('main.index'))
         flash('invalid username or password')
     return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Your have been logged out.')
+    return redirect(url_for('main.index'))
