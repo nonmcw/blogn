87c4ce56e2833bebc374044820abd7279b76f684
nonmcw <nonmcw@live.com>
2018-05-28 07:30:13 +0800

    add user profiles models.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
-
+from flask import render_template, session, redirect, url_for, flash, abort, current_app
+from flask_login import login_required, current_user
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
 from ..models import User
 
@@ -16,3 +16,23 @@ def user(username):
     if user is None:
         abort(404)
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm
+    if form.validate_on_submit:
+        current_user.name = form.name.data
+        current_user.phone = form.phone.data
+        currnet_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.phone.data = current_user.phone
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
