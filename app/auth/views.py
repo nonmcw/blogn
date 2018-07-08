2c1c9beddfa73b5ab2c1e210fd347687806977e3
nonmcw <nonmcw@live.com>
2018-07-08 21:26:45 +0800

    修改确认时的Bug

--- app/auth/views.py
+++ app/auth/views.py
@@ -73,7 +73,7 @@ def confirm(token):
 @login_required
 def resend_confirmation():
     token = current_user.generate_confirmation_token()
-    send_email(current_user.email, 'Confirm Your Account',
+    send_email(current_user.email, '确认账户',
                'auth/email/confirm', user=current_user, token=token)
     flash('A new confirmation email has been sent to you by email.')
     return redirect(url_for('main.index'))
